d018bf8f381e9c89315e9b774994af3c976aa2e3
Sakshi Jadhav <sakashijadhav9696@gmail.com>
2026-01-14 09:58:41 +0530

    Add files via upload

---
+++ DAY 4/Day4_whileLoop.py
@@ -0,0 +1,74 @@
+# #SQUARE OF N NUMBERS
+# num=int(input("Enter a number"))
+# i=1
+# while i<=num:
+#     print(i*i)
+#     i+=1
+
+
+
+
+# #DIVISIBLE BY 5
+# num=int(input("Enter a number"))
+# i=1
+# while i<=num:
+#     if i%5==0:
+#         print(i)
+#     i+=1
+
+
+
+# #SINGLE CHARACTER PRINT
+# str=input("Enter a string")
+# i=0
+# while i<len(str):
+#     print(str[i],end=" ")
+#     i=i+1
+
+# #DIVISIBLE BY 2 AND THEN COUNT
+# number=int(input("Enter Number:"))
+# i=1
+# count=0
+# while i<=number:
+#     if i%2==0:
+        
+#         count+=1
+#     i=i+1
+# print("TOTAL COUNT IS::")
+# print(count)
+
+
+#REVERSE A STRING
+string1=input("Enter A String")
+i=len(string1)-1
+
+while i>=0:
+    print(string1[i],end=" ")
+    i-=1
+
+
+
+
+# end=int(input("Enter end Number"))
+
+# i=2
+# if num<=1:
+#     print("not prime")
+
+# else:
+#     while i<=num:
+    
+#         if num%i==0:
+#             print("not prime",i)
+#         else:
+#             print("prime",i)
+#         i+=1
+
+    
+
+   
+
+
+
+  
+
