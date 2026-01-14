7eff403b08e88ad771ba87beccb9a7c330037139
Sakshi Jadhav <sakashijadhav9696@gmail.com>
2026-01-14 09:55:38 +0530

    Add files via upload

---
+++ DAY 2/looping_Day2.py
@@ -0,0 +1,85 @@
+#1. FOR LOOP PRINT 1 TO 10
+for i in range(1,11,1):
+    print(i)
+
+num=int(input("Enter number"))
+for i in range(num+1):
+        print(i)
+
+#SUM OF N NATURAL NUMBER
+sum=0
+num1=int(input("Enter number"))
+for i in range(num1+1):
+    sum=sum+i
+print(sum)
+
+
+# EVEN FROM RANGE
+
+for i in range(num1+1):
+    if i%2==0:
+        print(i)
+
+#print 6 table
+
+for i in range(1,11):
+    print("6X",i,"=",6*i)
+
+
+#FACTORIAL NUMBER
+fact=1
+num1=int(input("Enter number"))
+for i in range(1,num1+1):
+    fact=fact*i
+print(fact)
+
+#COUNT DIGITS IN A FOR LOOP
+
+
+
+#WHILE LOOP
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+i=1
+while i<=10:
+    print("6X",i,"=",6*i)
+    i=i+1
+
+#sum of n numbers
+sum=0
+i=0
+n=int(input("Enter Number "))
+while i<=n:
+    sum=sum+i;
+    i=i+1
+
+print(sum)
+
+
+#FACTORIAL NUMBER
+fact1=1
+i=1
+num2=int(input("Enter number"))
+while i<=num2:
+    fact1=fact1*i
+    i=i+1
+print(fact1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
---
+++ DAY 2/pattern_Day2.py
@@ -0,0 +1,17 @@
+ 
+for i in range(1,6):
+    print("*"*i)
+
+    
+
+i=1
+n=5
+while i<=n:
+    print("*"*i)
+    i=i+1
+
+
+    
+for i in range(5,0,-1):
+    print("*"*i)
+#use while loop
